65e6bf236487b1e0d3cb40db057a483fc0d3e887
Alejandro <aleagf@hotmail.com>
2025-07-14 13:06:26 -0400

    build fix

--- ecommerce_front/src/app/step1/page.tsx
+++
@@ -1,9 +0,0 @@
-import Step1Form from "@/modules/step1/step1Form";
-
-export default function Step1Page() {
-  return (
-    <main className="min-h-screen bg-gray-50 p-6">
-      <Step1Form />
-    </main>
-  );
-}
